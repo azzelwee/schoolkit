7ab4cf2f27b6f60f02131c53c4bd3c225a71dac4
Azzelswee <azzelswee@gmail.com>
2024-03-09 19:40:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-test 
+<?php
+
+$host ="localhost";
+$username = "root";
+$password = "12345";
+$database = "employee_system";
+
+$con = new mysqli($host, $username, $password, $database);
+
+if($con->connect_error){
+    echo $con->connect_error;
+}else{
+    echo "Connected";
+}
