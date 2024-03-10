8b873490da881a087a73f7d74e1d0f944c6e110f
Azzelswee <azzelswee@gmail.com>
2024-03-11 00:23:57 +0800

    Update 7

--- dashboard.php
+++ dashboard.php
@@ -33,52 +33,37 @@ $row = $employee->fetch_assoc();
     <div class="left-container">
     <h4>Main Navigation</h4></br>
         <ul class="category-list">
-            <li><img src="img/dashboard.png" alt=""><a href="index.php">Dashboard</a></li>
-            <li><img src="img/structure.png" alt=""><a href="">Department</a>
-            <li><img src="img/team.png" alt=""><a href="">Staff</a></li>
-            <li><img src="img/redo-arrow.png" alt=""><a href="">Leave</a></li>
+            <li><img src="img/dashboard.png" alt=""><a href="dashboard.php">Dashboard</a></li>
+            <li><img src="img/structure.png" alt=""><a href="#">Department</a>
+            <li><img src="img/team.png" alt=""><a href="#">Staff</a></li>
+            <li><img src="img/redo-arrow.png" alt=""><a href="#">Leave</a></li>
             <li><img src="img/exit.png" alt=""><a href="logout.php">Logout</a></li>  
         </ul> 
     </div>
 <!-- end of left container -->
 
     <div class="right-container">
-        <h2>Dashboard</h2>
+        <h2>Dashboard</h2></br>
 
-        <div class="search-container">
-        <form action="result.php" method="get">
-            <input type="text" name="search" id="search" class="search-input">
-            <button type="submit" class="search-button">search</button>
-        </form>
+        <div class="container-list">
+        <a href="index.php" class="container container1">
 
-        <table id= "myTable">
-        <thead>
-        <tr>
-            <th></th>
-            <th>First Name</th>
-            <th>Last Name</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php do{ ?>
-        <tr>
-            <td width="30"><a href="details.php?ID=<?php echo $row['id'];?>"
-            class="button-small">view</a></td>
-            <td><?php echo $row['first_name']; ?></td>
-            <td><?php echo $row['last_name']; ?></td>
-        </tr>
-        <?php }while($row = $employee->fetch_assoc()); ?>
-        </tbody>
-    </table>
+        </a>
 
-    <div class="bottom-navigation">
-        <button onclick="navigatePrevious()">Previous</button>
-        <span id="pageIndicator">Page 1</span>
-        <button onclick="navigateNext()">Next</button>
+        <a href="#" class="container container2">
+
+        </a>
+            
+        <a href="#" class="container container3">
+
+        </a>
+
+        <a href="#" class="container container4">
+
+        </a>
+
     </div>
     
-        
-
     </div>
     
     <script src="js/main.js"></script>
