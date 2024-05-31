c5709bc224c6df4dbcca162d5a6edb818bb84df0
Azzelswee <azzelswee@gmail.com>
2024-05-31 10:29:05 +0800

    update

--- addEmployee2.php
+++ addEmployee2.php
@@ -108,77 +108,85 @@ if(isset($_POST['submit'])) {
 
 <?php include 'header.php'; ?>
 
-    <div class="right-container-add">
-        <div class="box-container">
+<div class="right-container-add">
+    <div class="box-container">
         <div class="add-employee-container">
-    <h2>Add Employee</h2>
-    <form class="add-employee-form">
-        <div class="form-group">
-            <label for="employee-name">Name:</label>
-            <input type="text" id="employee-name" name="employee-name" required>
+            <h2>Basic Information</h2>
+            <form class="add-employee-form">
+                <div class="form-group small">
+                    <label for="employee-first-name">First Name:</label>
+                    <input type="text" id="employee-first-name" name="employee-first-name" required>
+                </div>
+                <div class="form-group small">
+                    <label for="employee-middle-name">Middle Name:</label>
+                    <input type="text" id="employee-middle-name" name="employee-middle-name" required>
+                </div>
+                <div class="form-group small">
+                    <label for="employee-last-name">Last Name:</label>
+                    <input type="text" id="employee-last-name" name="employee-last-name" required>
+                </div>
+                <div class="form-group">
+                    <label for="employee-email">Email:</label>
+                    <input type="email" id="employee-email" name="employee-email" required>
+                </div>
+                <div class="form-group small">
+                    <label for="employee-age">Age:</label>
+                    <input type="number" id="employee-age" name="employee-age" required>
+                </div>
+                <div class="form-group small">
+                    <label for="employee-gender">Gender:</label>
+                    <select id="employee-gender" name="employee-gender" required>
+                        <option value="">Select Gender</option>
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                        <option value="other">Other</option>
+                    </select>
+                </div>
+                <div class="form-group small">
+                    <label for="employee-height">Height (cm):</label>
+                    <input type="number" id="employee-height" name="employee-height" required>
+                </div>
+                <div class="form-group small">
+                    <label for="employee-weight">Weight (lbs.):</label>
+                    <input type="number" id="employee-weight" name="employee-weight" required>
+                </div>
+                <div class="form-group small">
+                    <label for="employee-birthdate">Birthdate:</label>
+                    <input type="date" id="employee-birthdate" name="employee-birthdate" required>
+                </div>
+                <div class="form-group small">
+                    <label for="employee-birthplace">Place of Birth:</label>
+                    <input type="text" id="employee-birthplace" name="employee-birthplace" required>
+                </div>
+                <div class="form-group small">
+                    <label for="employee-religion">Religion:</label>
+                    <input type="text" id="employee-religion" name="employee-religion" required>
+                </div>
+                <div class="form-group small">
+                    <label for="employee-civil-status">Civil Status:</label>
+                    <select id="employee-civil-status" name="employee-civil-status" required>
+                        <option value="">Select Status</option>
+                        <option value="single">Single</option>
+                        <option value="married">Married</option>
+                        <option value="divorced">Separated</option>
+                        <option value="widowed">Widowed</option>
+                    </select>
+                </div>
+                <div class="form-group small">
+                    <label for="employee-citizenship">Citizenship:</label>
+                    <input type="text" id="employee-citizenship" name="employee-citizenship" required>
+                </div>
+                <div class="form-group-below">
+                    <h2>Contact Information</h2>
+                </div>
+                <div class="form-group-button">
+                    <button type="submit">Add Employee</button>
+                </div>
+            </form>
         </div>
-        <div class="form-group">
-            <label for="employee-email">Email:</label>
-            <input type="email" id="employee-email" name="employee-email" required>
-        </div>
-        <div class="form-group">
-            <label for="employee-position">Position:</label>
-            <input type="text" id="employee-position" name="employee-position" required>
-        </div>
-        <div class="form-group small">
-            <label for="employee-age">Age:</label>
-            <input type="number" id="employee-age" name="employee-age" required>
-        </div>
-        <div class="form-group small">
-            <label for="employee-gender">Gender:</label>
-            <select id="employee-gender" name="employee-gender" required>
-                <option value="">Select Gender</option>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-                <option value="other">Other</option>
-            </select>
-        </div>
-        <div class="form-group small">
-            <label for="employee-height">Height (cm):</label>
-            <input type="number" id="employee-height" name="employee-height" required>
-        </div>
-        <div class="form-group small">
-            <label for="employee-weight">Weight (lbs.):</label>
-            <input type="number" id="employee-weight" name="employee-weight" required>
-        </div>
-        <div class="form-group small">
-            <label for="employee-birthdate">Birthdate:</label>
-            <input type="date" id="employee-birthdate" name="employee-birthdate" required>
-        </div>
-        <div class="form-group small">
-            <label for="employee-birthplace">Place of Birth:</label>
-            <input type="text" id="employee-birthplace" name="employee-birthplace" required>
-        </div>
-        <div class="form-group small">
-            <label for="employee-religion">Religion:</label>
-            <input type="text" id="employee-religion" name="employee-religion" required>
-        </div>
-        <div class="form-group small">
-            <label for="employee-civil-status">Civil Status:</label>
-            <select id="employee-civil-status" name="employee-civil-status" required>
-                <option value="">Select Status</option>
-                <option value="single">Single</option>
-                <option value="married">Married</option>
-                <option value="divorced">Separated</option>
-                <option value="widowed">Widowed</option>
-            </select>
-        </div>
-        <div class="form-group small">
-            <label for="employee-citizenship">Citizenship:</label>
-            <input type="text" id="employee-citizenship" name="employee-citizenship" required>
-        </div>
-        <div class="form-group">
-            <button type="submit">Add Employee</button>
-        </div>
-    </form>
+    </div>
 </div>
-</div>
-</div>
+
 
 <script src= js/main.js></script>
 
